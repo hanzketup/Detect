0e7c3e1378d4e8923425f5673f6d6021f20f048b
Hasse <hanneshermansson0000@gmail.com>
2018-07-05 17:20:57 +0200

    added mock key

--- backend/src/helpers/verifyRecaptcha.js
+++ backend/src/helpers/verifyRecaptcha.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 import FormData from 'form-data'
 import {recaptcha_secret} from '../keys'
+const test_secret = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
 
-export default async (token) => {
-
+export default async (token, test=false) => {
   let reData = new FormData()
-  reData.append('secret', recaptcha_secret)
+  reData.append('secret', test ? test_secret : recaptcha_secret)
   reData.append('response', token)
   let recap = await fetch('https://www.google.com/recaptcha/api/siteverify', {
     method: 'POST',
