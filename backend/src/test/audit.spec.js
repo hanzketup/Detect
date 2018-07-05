9fa864a7e9553c65fba0714d5a5628bca7843ed5
Hasse <hanneshermansson0000@gmail.com>
2018-07-05 22:48:24 +0200

    typo

--- backend/src/test/audit.spec.js
+++ backend/src/test/audit.spec.js
@@ -9,7 +9,7 @@ describe('Audit', function() {
 
   it('runs successfully', async function() {
     this.timeout(5000)
-    let audit_result = await audit.runAudit('https://example.com/')
+    let audit_result = await audit.runAudit('https://example.com/', true)
     assert.equal(audit_result.status, 'finished')
   })
 
