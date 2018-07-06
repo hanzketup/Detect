28ec9fba465a69a1a1cb4fc48359179b756b574a
Hasse <hanneshermansson0000@gmail.com>
2018-07-06 12:24:24 +0200

    added done() to async mocha tests

--- backend/src/test/audit.spec.js
+++ backend/src/test/audit.spec.js
@@ -1,20 +1,23 @@
 import assert from 'assert'
 import audit from '../audit'
 
-describe('Audit', function() {
+describe('Audit', function(done) {
 
   it('starts without crashing', async function() {
     await audit.begin()
+    done()
   })
 
-  it('runs successfully', async function() {
+  it('runs successfully', async function(done) {
     this.timeout(5000)
     let audit_result = await audit.runAudit('https://example.com/', true)
     assert.equal(audit_result.status, 'finished')
+    done()
   })
 
-  it('ends without crashing', function() {
+  it('ends without crashing', function(done) {
     audit.end()
+    done()
   })
 
 })
